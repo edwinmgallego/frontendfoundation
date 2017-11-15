9f2049df8e1dd9541cbb13164f4263c22e0b39ff
edwinmgallego <ing.edwin.gallego@gmail.com>
2017-11-15 14:40:35 -0500

    data sticky

--- foundation/roldice/js/app.js
+++ foundation/roldice/js/app.js
@@ -1,1 +1,6 @@
 $(document).foundation()
+$('[data-toggle-dia]').click(function onClick(ev){
+    const panel= $(this).data('toggleDia')
+   $('#lineup-tabs').foundation('selectTab',panel)
+    
+})
