ce359472f51592b7cf679a99ae010040d884cecd
edwinmgallego <ing.edwin.gallego@gmail.com>
2017-11-27 21:45:24 -0500

    creacion del  offcanvas

--- foundation/roldice/js/app.js
+++ foundation/roldice/js/app.js
@@ -4,3 +4,12 @@ $('[data-toggle-dia]').click(function onClick(ev){
    $('#lineup-tabs').foundation('selectTab',panel)
     
 })
+const $offCanvas=$('#offCanvas')
+const $sticky=$('#sticky')
+$offCanvas.find('li').click(function(ev){
+   $offCanvas.foundation('close')    
+    setTimeout(function(){
+      $sticky.css('left','0px')   
+    },300)
+   
+})
